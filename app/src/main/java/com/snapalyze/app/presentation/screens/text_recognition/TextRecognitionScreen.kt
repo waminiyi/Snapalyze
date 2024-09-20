ba512237226c4c17258e6acb6f6d345045bc40c1
Patrick S <dev@sotopatrick.com>
2024-09-20 22:41:04 +0200

    Text recognition implemented

--- app/src/main/java/com/snapalyze/app/presentation/screens/text_recognition/TextRecognitionScreen.kt
+++ app/src/main/java/com/snapalyze/app/presentation/screens/text_recognition/TextRecognitionScreen.kt
@@ -1,11 +1,21 @@
 package com.snapalyze.app.presentation.screens.text_recognition
 
 
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -15,13 +25,25 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionRequired
+import com.google.accompanist.permissions.rememberPermissionState
 import com.snapalyze.app.R
+import com.snapalyze.app.analyzer.TextRecognitionAnalyzer
+import com.snapalyze.app.presentation.common.components.CameraView
 
 object TextRecognitionScreen : Screen {
 
@@ -51,15 +73,89 @@ object TextRecognitionScreen : Screen {
                 },
                 contentColor = MaterialTheme.colorScheme.primary,
                 content = { padding ->
-                    Text(
-                        text = stringResource(id = R.string.text_recognition_title),
-                        style = MaterialTheme.typography.titleLarge,
-                        modifier = Modifier
-                            .align(Alignment.Center)
-                            .padding(padding)
-                    )
+                    TextRecognitionScreenContent()
                 }
             )
+        }
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun TextRecognitionScreenContent() {
+    val context = LocalContext.current
+    val cameraPermissionState =
+        rememberPermissionState(permission = android.Manifest.permission.CAMERA)
+
+    PermissionRequired(
+        permissionState = cameraPermissionState,
+        permissionNotGrantedContent = {
+            LaunchedEffect(Unit) {
+                cameraPermissionState.launchPermissionRequest()
+            }
+        },
+        permissionNotAvailableContent = {
+            Column {
+                Toast.makeText(context, "Permission denied.", Toast.LENGTH_LONG).show()
+            }
+        },
+        content = {
+            ScanSurface()
+        }
+    )
+}
+
+@Composable
+fun ScanSurface() {
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val extractedText = remember { mutableStateOf("") }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        CameraView(
+            context = context,
+            lifecycleOwner = lifecycleOwner,
+            analyzer = TextRecognitionAnalyzer {
+                if (it != extractedText.value && it.isNotBlank()) {
+                    extractedText.value = it
+                }
+            }
+        )
+        Column(
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxHeight()
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = stringResource(id = R.string.text_recognition_title),
+                    style = MaterialTheme.typography.titleLarge,
+                    color = Color.White
+                )
+            }
+
+            Card(
+                shape = RoundedCornerShape(12.dp),
+                elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+                colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primaryContainer),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
+            ) {
+                Text(
+                    text = extractedText.value,
+                    style = MaterialTheme.typography.titleSmall,
+                    modifier = Modifier
+                        .padding(horizontal = 10.dp)
+                        .align(Alignment.CenterHorizontally)
+                )
+            }
         }
     }
 }
